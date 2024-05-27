b842220fc6d5dacf60e2d530bb94208564a6f04c
Sivakumar S <SivakumarS@HCLTech0203.onmicrosoft.com>
2024-05-27 23:02:49 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SKSM_WebApp1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
